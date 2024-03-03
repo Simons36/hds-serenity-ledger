253ed44eceedfef557b068e2c3a41d7c9831050e
Simão Silva <email@simaosilva.com>
2024-03-03 19:05:38 +0000

    Added kitty dependency to dependency installer

--- HDSLedger/install_deps.sh
+++ HDSLedger/install_deps.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
 mvn install:install-file -Dfile=dependencies/gson-2.10.1.jar -DgroupId="com.google.code.gson" -DartifactId="gson" -Dversion="2.10.1" -Dpackaging="jar"
+
+# Install Kitty terminal; if you are using a apt package manager, uncomment the following line and comment the dnf line
+#sudo apt install kitty
+
+sudo dnf install kitty
