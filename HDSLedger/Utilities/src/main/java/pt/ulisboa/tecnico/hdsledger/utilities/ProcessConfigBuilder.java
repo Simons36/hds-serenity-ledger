6c068837e01265675f6e56616c208f74b2b77df5
Simão Silva <email@simaosilva.com>
2024-03-20 10:16:08 +0000

    Removed unnecessary print

--- HDSLedger/Utilities/src/main/java/pt/ulisboa/tecnico/hdsledger/utilities/ProcessConfigBuilder.java
+++ HDSLedger/Utilities/src/main/java/pt/ulisboa/tecnico/hdsledger/utilities/ProcessConfigBuilder.java
@@ -14,7 +14,7 @@ public class ProcessConfigBuilder {
     private final ProcessConfig instance = new ProcessConfig();
 
     public ProcessConfig[] fromFile(String path) {
-        System.out.println(path);
+        // System.out.println(path);
         try (BufferedInputStream is = new BufferedInputStream(new FileInputStream(path))) {
             String input = new String(is.readAllBytes(), StandardCharsets.UTF_8);
             Gson gson = new Gson();
