99822483451df0ace83519354abc02cb84937706
Simão Silva <email@simaosilva.com>
2024-03-31 22:37:58 +0100

    commit

--- HDSLedger/install_deps.sh
+++ HDSLedger/install_deps.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 mvn install:install-file -Dfile=dependencies/gson-2.10.1.jar -DgroupId="com.google.code.gson" -DartifactId="gson" -Dversion="2.10.1" -Dpackaging="jar"
+mvn install:install-file -Dfile=dependencies/junit-4.13.2.jar -DgroupId="junit" -DartifactId="junit" -Dversion="4.13.2" -Dpackaging="jar"
 
 # Install Kitty terminal; if you are using a apt package manager, uncomment the following line and comment the dnf line
 #sudo apt install kitty
 
-sudo dnf install kitty
+#sudo dnf install kitty
